ef5e4d65ee2101fcf160b4387a6988529eadcdc7
Soufianetyk1337 <sz.fanel@gmail.com>
2022-07-20 23:48:27 +0100

    Global CSS and refactoring index file

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  if (typeof window === 'undefined') {
+    return <></>
+  } else {
+    return <Component {...pageProps} />
+  }
 }
 
 export default MyApp
