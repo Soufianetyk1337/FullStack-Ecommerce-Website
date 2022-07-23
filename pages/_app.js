3b6ac97dcc2ede2c31cc8357e39db8ad3403a8ba
Soufianetyk1337 <sz.fanel@gmail.com>
2022-07-23 03:29:05 +0100

    Adding SafeHydration

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,34 @@
+import { useEffect, useState } from 'react';
+import Layout from '../containers/Layout'
+import StoreContext from '../context/StoreContext'
 import '../styles/globals.css'
 
+function SafeHydrate({ children }) {
+  return (
+    <div>
+      {typeof window === 'undefined' ? null : children}
+    </div>
+  )
+}
+
 function MyApp({ Component, pageProps }) {
-  if (typeof window === 'undefined') {
-    return <></>
-  } else {
-    return <Component {...pageProps} />
+  const [showChild, setShowChild] = useState(false);
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
+
+  if (!showChild) {
+    return null;
   }
+  return (
+    <StoreContext>
+      <SafeHydrate>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SafeHydrate>
+    </StoreContext>
+  )
 }
 
 export default MyApp
