287a27f562a3a6789902b6d32e6a2c6d394ec704
Soufianetyk1337 <sz.fanel@gmail.com>
2022-07-23 03:28:11 +0100

    Updating schemaTypes

--- sanity-cms/schemas/schema.js
+++ sanity-cms/schemas/schema.js
@@ -3,10 +3,11 @@ import createSchema from 'part:@sanity/base/schema-creator'
 
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 import product from './product'
+import banner from './banner'
 export default createSchema({
   name: 'default',
   types: schemaTypes.concat([
-    product
-
+    product,
+    banner
   ]),
 })
