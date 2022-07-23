87dbb86b6787fa16b0b025d89a43e248eaa0faaa
Soufianetyk1337 <sz.fanel@gmail.com>
2022-07-23 03:26:33 +0100

    Deleting nav menu from navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,16 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable @next/next/no-html-link-for-pages */
 import Image from "next/image";
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const Navbar = () => {
+  const cartRef = useRef(null);
+  const [toggle, setToggle] = useState(false);
   return (
     <header className="px-4">
       <div className="md:container lg:max-w-screen-xl mx-auto md:border-b md:relative">
-        <div className="flex items-center h-20 md:items-center">
-          <button className="js-nav-btn text-gray-600 md:hidden">
+        <div className="flex items-center content-between h-20 md:items-center">
+          <button className="text-gray-600 md:hidden">
             <i className="icon-menu"></i>
             <span className="sr-only">Menu</span>
           </button>
@@ -23,49 +26,32 @@ const Navbar = () => {
               height="20"
             />
           </a>
-          <div className="js-nav js-nav-btn fixed inset-0 z-50 bg-black bg-opacity-50 hidden"></div>
-          <nav className="js-nav fixed top-0 left-0 w-2/3 h-screen z-50 bg-white px-5 hidden md:static md:block md:w-auto md:h-auto md:mx-auto">
-            <button className="js-nav-btn text-gray-600 h-20 md:hidden">
-              <i className="icon-close"></i>
-              <span className="sr-only">Close</span>
-            </button>
-            <div className="mt-8 flex flex-col space-y-4 font-bold text-lg md:flex-row md:space-y-0 md:space-x-4 md:mt-0 md:h-full">
-              <a href="#" className="nav-link">
-                Collections
-              </a>
-              <a href="#" className="nav-link">
-                Men
-              </a>
-              <a href="#" className="nav-link">
-                Women
-              </a>
-              <a href="#" className="nav-link">
-                About
-              </a>
-              <a href="#" className="nav-link">
-                Contact
-              </a>
-            </div>
-          </nav>
-          <div className="md:flex md:items-center group">
-            <button className="js-basket-btn text-lg relative text-gray-600">
+          <div className="fixed inset-0 z-50 bg-black bg-opacity-50 hidden"></div>
+          <div
+            ref={cartRef}
+            onClick={() => {
+              console.log(cartRef.current);
+            }}
+            className="md:flex md:items-center group"
+          >
+            <button className=" text-lg relative text-gray-600">
               <i
                 className="bx bx-cart"
                 style={{ fontWeight: "bold", fontSize: "21px" }}
               ></i>
-              <span className="js-basket-qty absolute -top-2 -right-4 w-6 bg-orange-600 text-white text-xs rounded-full hidden"></span>
+              <span className=" absolute -top-2 -right-4 w-6 bg-orange-600 text-white text-xs rounded-full hidden"></span>
               <span className="sr-only">Cart</span>
             </button>
-            <div className="js-basket absolute top-24 left-0 z-40 w-full hidden md:block md:w-96 md:right-0 md:left-auto md:opacity-0 md:pointer-events-none md:group-hover:opacity-100 md:group-hover:pointer-events-auto transition">
+            <div className=" absolute top-20 left-0 z-40 w-full hidden md:block md:w-96 md:right-0 md:left-auto md:opacity-0 md:pointer-events-none md:group-hover:opacity-100 md:group-hover:pointer-events-auto transition">
               <div className="bg-white rounded-lg shadow-xl mx-2">
                 <div className="px-4 py-6 border-b">
                   <strong>Cart</strong>
                 </div>
-                <div className="js-basket-empty px-4 h-36 flex justify-center items-center">
+                <div className="px-4 h-36 flex justify-center items-center">
                   <strong className="text-gray-500">Your cart is empty</strong>
                 </div>
-                <template className="js-basket-template">
-                  <div className="js-basket-item flex items-center mb-4">
+                <template>
+                  <div className=" flex items-center mb-4">
                     <div className="w-16 h-auto rounded-lg mr-4">
                       <Image
                         src="/images/image-product-1-thumbnail.jpg"
@@ -78,20 +64,19 @@ const Navbar = () => {
                       <p>%_name_%</p>
                       <p>
                         <span>
-                          $%_price_% &times;{" "}
-                          <span className="js-basket-item-qty">%_qty_%</span>
+                          $%_price_% &times; <span className="">quantity</span>
                         </span>
-                        <span className="js-basket-item-total text-black font-bold">
+                        <span className=" text-black font-bold">
                           $%_total_%
                         </span>
                       </p>
                     </div>
-                    <button className="js-basket-remove ml-auto text-gray-500">
+                    <button className=" ml-auto text-gray-500">
                       <i className="icon-delete"></i>
                     </button>
                   </div>
                 </template>
-                <div className="js-basket-content px-4 py-6 flex flex-col hidden">
+                <div className="px-4 py-6 flex flex-col hidden">
                   <button className="w-full h-14 bg-orange-600 text-white font-medium rounded-lg order-1">
                     Checkout
                   </button>
@@ -99,17 +84,6 @@ const Navbar = () => {
               </div>
             </div>
           </div>
-          <button className="ml-6">
-            <div className="h-8 w-8 md:h-12 md:w-12 border-2 rounded-full hover:border-orange-600 transition relative">
-              <Image
-                src="/images/image-avatar.png"
-                alt="User avatar"
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-            <span className="sr-only">Avatar</span>
-          </button>
         </div>
       </div>
     </header>
