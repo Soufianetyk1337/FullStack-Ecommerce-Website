90d40c681c13e0bc338caf556025913f99132da9
Soufianetyk1337 <sz.fanel@gmail.com>
2022-07-23 03:26:55 +0100

    Updating components exports

--- components/index.js
+++ components/index.js
@@ -1,3 +1,5 @@
 export { default as Product } from './Product'
 export { default as Navbar } from './Navbar'
 export { default as ProductImagesSlider } from './ProductImagesSlider'
+export { default as SingleProduct } from './SingleProduct'
+export { default as HeaderBanner } from './HeaderBanner'
