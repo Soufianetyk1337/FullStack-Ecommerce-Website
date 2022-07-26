02fc7d31dae6e54463e05e3c12bdc01142cee375
Soufianetyk1337 <sz.fanel@gmail.com>
2022-07-26 04:33:26 +0100

    Displaying Quantity number next to cart icon

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,17 +1,22 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @next/next/no-html-link-for-pages */
 import Image from "next/image";
-import React, { useRef, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
+import { Store } from "../context/StoreContext";
+import Cart from "./Cart";
 
 const Navbar = () => {
+  const { totalQuantities, toggleShowCart, setToggleShowCart } =
+    useContext(Store);
+
   const cartRef = useRef(null);
   const [toggle, setToggle] = useState(false);
   return (
     <header className="px-4">
       <div className="md:container lg:max-w-screen-xl mx-auto md:border-b md:relative">
-        <div className="flex items-center content-between h-20 md:items-center">
+        <div className="flex items-center justify-between h-20 md:items-center">
           <button className="text-gray-600 md:hidden">
-            <i className="icon-menu"></i>
+            <i className="bx bx-menu text-2xl font-bold"></i>
             <span className="sr-only">Menu</span>
           </button>
           <a
@@ -27,65 +32,25 @@ const Navbar = () => {
             />
           </a>
           <div className="fixed inset-0 z-50 bg-black bg-opacity-50 hidden"></div>
-          <div
-            ref={cartRef}
-            onClick={() => {
-              console.log(cartRef.current);
-            }}
-            className="md:flex md:items-center group"
-          >
-            <button className=" text-lg relative text-gray-600">
-              <i
-                className="bx bx-cart"
-                style={{ fontWeight: "bold", fontSize: "21px" }}
-              ></i>
+          <div className="md:flex md:items-center group">
+            <button
+              className=" text-lg relative text-gray-600"
+              onClick={() => {
+                console.log("Clicked", toggleShowCart);
+                setToggleShowCart(!toggleShowCart);
+              }}
+            >
+              <i className="bx bx-cart text-2xl font-bold"></i>
               <span className=" absolute -top-2 -right-4 w-6 bg-orange-600 text-white text-xs rounded-full hidden"></span>
               <span className="sr-only">Cart</span>
+              <span className="font-semibold text-xs absolute bg-orange-300 rounded-full text-white right-[-12px] top-[-6px] w-[18px] h-[18px]">
+                {totalQuantities}
+              </span>
             </button>
-            <div className=" absolute top-20 left-0 z-40 w-full hidden md:block md:w-96 md:right-0 md:left-auto md:opacity-0 md:pointer-events-none md:group-hover:opacity-100 md:group-hover:pointer-events-auto transition">
-              <div className="bg-white rounded-lg shadow-xl mx-2">
-                <div className="px-4 py-6 border-b">
-                  <strong>Cart</strong>
-                </div>
-                <div className="px-4 h-36 flex justify-center items-center">
-                  <strong className="text-gray-500">Your cart is empty</strong>
-                </div>
-                <template>
-                  <div className=" flex items-center mb-4">
-                    <div className="w-16 h-auto rounded-lg mr-4">
-                      <Image
-                        src="/images/image-product-1-thumbnail.jpg"
-                        alt="%_name_%"
-                        layout="fill"
-                        objectFit="cover"
-                      />
-                    </div>
-                    <div className="text-gray-600">
-                      <p>%_name_%</p>
-                      <p>
-                        <span>
-                          $%_price_% &times; <span className="">quantity</span>
-                        </span>
-                        <span className=" text-black font-bold">
-                          $%_total_%
-                        </span>
-                      </p>
-                    </div>
-                    <button className=" ml-auto text-gray-500">
-                      <i className="icon-delete"></i>
-                    </button>
-                  </div>
-                </template>
-                <div className="px-4 py-6 flex flex-col hidden">
-                  <button className="w-full h-14 bg-orange-600 text-white font-medium rounded-lg order-1">
-                    Checkout
-                  </button>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
       </div>
+      {toggleShowCart && <Cart />}
     </header>
   );
 };
