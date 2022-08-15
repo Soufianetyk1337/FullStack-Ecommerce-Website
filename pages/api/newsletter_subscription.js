0e8c8333ce1ca1e5c2e758fcaefe2fed98ffd120
Soufianetyk1337 <sz.fanel@gmail.com>
2022-08-15 21:21:30 +0100

    Sending Email Api route

---
+++ pages/api/newsletter_subscription.js
@@ -0,0 +1,35 @@
+import nodemailer from "nodemailer";
+
+export default async function handler(req, res) {
+    try {
+        const email = req.body;
+        const mailOptions = {
+            from: '"Nike" <john-doe@gmail.com>',
+            to: email,
+            subject: "Newsletter subscription",
+            html: "<h1>Success</h1>",
+        };
+        const transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
+            service: "gmail",
+            auth: {
+                user: process.env.NODEMAILER_USER,
+                pass: process.env.NODEMAILER_PASS,
+            },
+        });
+        const mailSent = await transporter.sendMail(mailOptions);
+        if (mailSent) {
+            return res.status(200).json({
+                success: "true",
+                message:
+                    "You have successfully subscribed to our newsletter we'll keep by updated with our news and new products",
+            });
+        } else {
+            return res.status(409).json({ mailSent });
+        }
+    } catch (error) {
+        return res.status(409).json({ success: false, message: error.message });
+    }
+}
