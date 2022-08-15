d3f3d05469e005e4f95df0f275e9b237cd9fb8f4
Soufianetyk1337 <sz.fanel@gmail.com>
2022-08-15 21:23:06 +0100

    Linking Poppins fonts

--- pages/_document.js
+++ pages/_document.js
@@ -3,14 +3,13 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
     render() {
         return (
-            <Html>
+            <Html className='scroll-smooth' lang='en'>
                 <Head>
                     <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet' />
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-                    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;500;600&display=swap" rel="stylesheet" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" as='font' />
+                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet" />
                 </Head>
-                <body>
+                <body className='font-Poppins'>
                     <Main />
                     <NextScript />
                 </body>
