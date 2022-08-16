11956bcc19cfb3d03911586cfc2d97bfb2fe3934
Soufianetyk1337 <sz.fanel@gmail.com>
2022-08-16 13:55:00 +0100

    Fixing STRIPE env value typo

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    STIPE_SECRET_KEY: process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY,
-    STIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
   },
   images: {
     domains: ["cdn.sanity.io"],
